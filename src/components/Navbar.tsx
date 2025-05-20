0fb0508b98f6648ecb98880296148f1d759f7b40
Eduard177 <eduarro2001@gmail.com>
2025-05-20 18:55:41 -0400

    feat: add SVG logo and update Navbar component to use it
    - Added Logo.svg asset for branding.
    - Updated Navbar component to replace the image with the new SVG logo.
    - Added type definitions for SVG imports.
    - Updated package.json to include necessary dependencies for SVG handling.
    - Adjusted tsconfig.json to include new types and source paths.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import type { NavigationProp } from '@react-navigation/native'; // ajusta el path según tu proyecto
 import { RootStackParamList } from '../navigation';
+import Logo from '../assets/Logo.svg';
 
 interface NavbarProps {
   onFilter: () => void;
@@ -15,12 +16,11 @@ export default function Navbar({ onFilter, onReload }: Readonly<NavbarProps>) {
   return (
     <View className="flex flex-row justify-between items-center p-4 pt-12 bg-white dark:bg-black border-b border-gray-300 dark:border-gray-700">
       {/* Logo */}
-      <Image
-        source={require('../assets/icon.png')}
-        style={{ width: 40, height: 40 }}
-        resizeMode="contain"
-        className="rounded-full"
-      />
+      <View>
+        <Pressable onPress={() => navigation.navigate('Home')}>
+               <Logo />
+        </Pressable>
+      </View>
 
       {/* Botones */}
       <View className="flex-row space-x-4">
